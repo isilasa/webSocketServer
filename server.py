76fbf01fef21a2e17912f4b8f65d8105efe65b95
isilasa <isilasaquitall@gmail.com>
2019-12-22 15:16:43 +0300

    WebServer

--- server.py
+++ server.py
@@ -1,38 +1,18 @@
-import socket
+import asyncio
+import websockets
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-clients = []
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
 
 
-def start_server(port):
-    host = socket.gethostname()
-    sock.bind((host, int(port)))
-    print("Server start\t")
-    sock.listen(5)
-    print("Listen\t")
-    return port
+start_server = websockets.serve(hello, "localhost", 8765)
 
-
-start_server(12345)
-
-while True:
-    conn, addr = sock.accept()
-    print("адрес подключения", addr)
-    conn.send("You are connect".encode("utf-8"))
-    data = conn.recv(2048)
-    print("msg from client: " + str(data))
-
-    if addr != clients:
-        clients.append(addr)
-        print(clients)
-
-    for client in clients:
-        if client != addr:
-            conn.sendto(data, addr)
-        else:
-            print("данные else", data.decode("utf-8"))
-
-    if str(data) == "Stop":
-        print("Connection close")
-        break
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
